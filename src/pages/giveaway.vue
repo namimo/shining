0ef290bd01029dd53af735dcf491eeb0c1193347
Namus <sumaniwaram@gmail.com>
2021-02-04 21:17:55 +0545

    add: provide head to giveaway page

--- src/pages/giveaway.vue
+++ src/pages/giveaway.vue
@@ -1,1 +1,14 @@
+<script lang="ts">
+import { defineComponent } from "vue"
+import { useHead } from "@vueuse/head"
+
+export default defineComponent({
+  setup() {
+    useHead({
+      title: "Giveaway Sign up",
+    })
+  },
+})
+</script>
+
 <template>Give</template>

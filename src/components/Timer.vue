c100001febd9dbfd99759b97ed16b08cbe954f10
Namimo <namusasmimo@gmail.com>
2021-02-14 21:51:21 +0545

    refactor: make responsive

--- src/components/Timer.vue
+++ src/components/Timer.vue
@@ -1,13 +1,15 @@
 <template>
   <div class="absolute inset-0 w-full h-full">
     <div class="flex items-center justify-center w-full h-full">
-      <div class="px-6 py-5 rounded-lg shadow-xl bg-gradient-to-b to-coolGray-900 from-red-600">
+      <div
+        class="px-3 py-4 rounded-lg shadow-xl sm:py-5 w-44 sm:w-auto md:px-6 bg-gradient-to-b to-coolGray-900 from-red-600"
+      >
         <div
-          class="px-8 py-3 mx-auto -mt-8 text-xl tracking-widest text-center text-white uppercase bg-red-600 rounded-lg shadow-md shake-vertical max-w-max"
+          class="px-2 py-3 mx-auto -mt-8 text-lg tracking-widest text-center uppercase bg-red-600 rounded-lg shadow-md sm:text-xl sm:px-8 max-w-max"
         >
           Expires On
         </div>
-        <p class="mt-3 text-6xl font-medium tracking-tighter text-white">00 : 00 : 00</p>
+        <p class="mt-3 text-2xl font-medium tracking-tighter sm:text-6xl">00 : 00 : 00</p>
       </div>
     </div>
   </div>

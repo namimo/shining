13c2000f2c02452c85e754fe801ef6eb257bb10a
Namimo <namusasmimo@gmail.com>
2021-02-23 23:41:36 +0545

    refactor: increase max-width

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,7 @@ import Footer from "./components/Footer.vue"
 <template>
   <div class="flex flex-col min-h-screen">
     <Particles />
-    <main class="relative flex-grow max-w-xl px-4 py-8 mx-auto sm:py-12 lg:py-16">
+    <main class="relative flex-grow w-full max-w-5xl px-4 py-8 mx-auto sm:py-12 lg:py-16">
       <router-view />
     </main>
     <Footer class="flex-shrink-0" />

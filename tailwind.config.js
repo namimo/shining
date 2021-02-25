0efc919f23b2e7226bbb1ad5d4d77e654ff3d504
Namimo <namusasmimo@gmail.com>
2021-02-25 14:58:32 +0545

    refactor: move 'button-primary' class to config and clear animation css

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,10 @@ const colors = require("windicss/colors")
 const { theme } = require("windicss/defaultConfig")
 
 module.exports = {
+  shortcuts: {
+    "button-primary":
+      "inline-flex items-center justify-center px-12 py-3 text-lg font-semibold tracking-wide rounded-full bg-gradient-to-br from-lightBlue-500 to-gray-700 lg:text-3xl focus:outline-none",
+  },
   theme: {
     extend: {
       // ...
